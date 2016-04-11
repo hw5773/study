2dca628f3ff6a4f4526bd548aa0bf49d436854f5
Hyunwoo Lee <hw5773@gmail.com>
2016-04-11 13:40:49 +0900

    modify ga

---
+++ genetic_algorithm/hw1/mock.c
@@ -0,0 +1,36 @@
+int init_population()
+{
+	return 0;
+}
+
+int selection(int *p1, int *p2)
+{
+	return 0;
+}
+
+int crossover(int i, int p1, int p2)
+{
+	return 0;
+}
+
+int mutation(int i)
+{
+	return 0;
+}
+
+int replacement(void)
+{
+	return 0;
+}
+
+int stop_condition()
+{
+	return 0;
+}
+
+int free_population(void)
+{
+	return 0;
+}
+
+
