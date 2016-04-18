4056681bd1df8b48fab743ffe5e2457ab2b72b70
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2016-04-18 15:12:39 +0900

    multi crossover

---
+++ genetic_algorithm/hw1/multi_point_crossover.c
@@ -0,0 +1,26 @@
+#ifndef __GENETIC_H__
+#define __GENETIC_H__
+#include "genetic.h"
+#endif
+
+int init_crossover(int point[])
+{
+	
+
+// i: the index number of the offspring
+// p1, p2: the index number of the parents
+int crossover(int i, int p1, int p2)
+{
+	init_chromosome(&offsprings[i]);
+
+	int n=0, m=0;
+	for (n=1; n<=SIZE; n++)
+	{
+		for (m=0; m<=POINTS; m++)
+		{
+			// TODO: 
+		}
+	}
+
+	return 1;
+}
