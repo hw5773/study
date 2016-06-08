4366b1b3c6b687c8ee563344dd25b67cd3638074
Hyunwoo Lee <hw5773@gmail.com>
2016-06-08 19:17:07 +0900

    modify genetic.h

--- genetic_algorithm/binary/genetic.h
+++ genetic_algorithm/binary/genetic.h
@@ -7,8 +7,8 @@
 #define SIZE 		num_of_vertex
 #define S_RATE		0.7
 #define M_THRE		0.03
-#define N			200
-#define K			3
+#define N			10
+#define K			1
 #define P0			0.70
 
 #define POINTS		4
@@ -16,6 +16,8 @@
 #define K_TOR		2
 #define T			0.68	// the threshold of the tornament selection
 #define C			0.5 // the threshold of the reverse selection
+
+#define RANGE		500
 
 FILE 			*in, *out;
 extern int 		num_of_vertex;
@@ -39,14 +41,14 @@ Chromosome 		*offsprings[K+1];	// The array of the offsprings
 extern Chromosome		*max;
 
 // if the stop condition is satisfied then returns 1, if not, returns 0
-int 			stop_condition(void); 
+int 			stop_condition(int[][SIZE+1]); 
 
 // operations in GA
 // returns 1 if the selection is done rightly. returns -1 if fails
 // p1, p2 are the index of the parents in the population
 // i is the index of the offspring in the offsprings
 int				selection(int *p1, int *p2); 
-int 			crossover(int i, int p1, int p2);
+int 			crossover(int i, int p1, int p2, int edge[][SIZE+1]);
 int 			mutation(int i);
 int 			replacement(int edge[][SIZE+1]);
 int				init_crossover(void);
@@ -54,12 +56,12 @@ int				local_optimization(int, int[][SIZE+1]);
 
 // the functions related to the representation.
 unsigned long 	get_nano_seconds(void);
-int 			init_population(void);
+int 			init_population(int [][SIZE+1]);
 int 			free_population(void);
-int 			init_offsprings(void);
+int 			init_offsprings(int[][SIZE+1]);
 int 			free_offsprings(void);
 int				init_cost(int edge[][SIZE+1]);
-int 			init_chromosome(Chromosome **c);
+int 			init_chromosome(Chromosome **c, int[][SIZE+1]);
 int 			free_chromosome(Chromosome *c);
 int				sort_population(void);
 void 			print_chromosome(Chromosome *c);
