2e0afd2e5596557a60853141325a90a6aa9a4cbf
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2017-03-07 22:55:21 +0900

    Add port scanner

---
+++ cert/port.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import struct
+import socket
+import subprocess
+import time
+
+def usage():
+    print ("Full IPv4 Scanning to find the hosts which open the 443 port.")
+    print ("python3 port.py <output filename> <min int> <max int>")
+    exit(1)
+
+def range_error():
+    print ("max_int must bigger or equal to min_int")
+    print ("python3 port.py <output filename> <min int> <max int>")
+    exit(1)
+
+def full_scan(min_int, max_int):
+    ip_lst = []
+    
+    for addr in range(min_int, max_int+1):
+        ip = socket.inet_ntoa(struct.pack("!I", addr))
+        process = subprocess.Popen(["nmap", "-p443", "-Pn", ip], stdout=subprocess.PIPE)
+        output = process.communicate()[0].decode('ascii').replace("\t", "").split("\n")
+
+        if "443/tcp open  https" in output:
+            ip_lst.append(ip)
+
+    return ip_lst
+
+def main():
+    if len(sys.argv) != 4:
+        usage()
+
+    of = open(sys.argv[1], "w")
+    min_int = int(sys.argv[2])
+    max_int = int(sys.argv[3])
+
+    if min_int > max_int:
+        range_error()
+
+    start = time.time()
+    print ("start: ", start)
+    ip_lst = full_scan(min_int, max_int)
+    end = time.time()
+    print ("end: ", end)
+    print ("elapsed time: ", end - start)
+    print ("total ip addresses: ", len(ip_lst))
+
+    for ip in ip_lst:
+        of.write(ip)
+
+    of.close()
+
+if __name__ == "__main__":
+    main()
